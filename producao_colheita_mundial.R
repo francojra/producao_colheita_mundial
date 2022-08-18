a41a4fccbca80f8b890b4eb98064c0f88e21143f
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-17 23:39:23 -0300

    Add files via upload

---
+++ producao_colheita_mundial.R
@@ -0,0 +1,14 @@
+
+# Produção de colheita no mundo ------------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 16/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/transport -----------------------------------------------------------------------------------------
+
+# Sobre o conjunto de dados ----------------------------------------------------------------------------------------------------------------
+
+### Dados recentes sobre produção de colheita no mundo, baseado sobre
+### estimativas publicadas pelo Food and Agriculture Organization (FAO).
+
+### O FAO disponibiliza esses dados por país e região desde 1961. Os
+### valores de produção são comunicados em toneladas.
+ 
