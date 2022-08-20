fd52e00860926d11a83012c977e883a38d7d6fc4
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-20 03:48:27 -0300

    Add files via upload

--- producao_colheita_mundial.R
+++ producao_colheita_mundial.R
@@ -17,6 +17,7 @@
 library(tidyverse)
 library(scales)
 library(gridExtra)
+library(pals)
 
 # Carregar dados ---------------------------------------------------------------------------------------------------------------------------
 
@@ -34,12 +35,13 @@ prod <- prod %>%
 prod1 <- prod %>%
   filter(Entity %in% c("Brazil", "Peru", "Mexico", "Venezuela",
          "Nicaragua", "Honduras", "China", "India", "Indonesia",
-         "Ethiopia", "Vietnam")) %>%
+         "Ethiopia", "Vietnam", "Guatemala", "Nicaragua",
+         "Colombia", "Uganda", "Madagascar")) %>%
   view()
 
 prod2 <- prod1 %>%
-  filter(Entity %in% c("Brazil",
-         "Honduras", "Indonesia",
+  filter(Entity %in% c("Brazil", "Colombia", 
+         "Mexico", "Indonesia",
          "Vietnam")) %>%
   view()
 
@@ -51,23 +53,23 @@ g1 <- ggplot(prod1, aes(x = fct_reorder(Entity, producao),
                        y = producao, fill = Entity)) +
   geom_col() +
   coord_flip() +
-  scale_fill_brewer(palette = "Paired") +
+  scale_fill_manual(values = as.vector(alphabet(16))) +
   scale_y_continuous(labels = comma, 
                      expand = expansion(mult = c(0, .1))) +
   labs(x = "País", y = "Produção de café (t)") +
-  theme_minimal(base_size = 15) +
+  theme_minimal(base_size = 13) +
   theme(legend.position = "none")
 g1
 
 g2 <- ggplot(prod2, aes(Year, producao, color = Entity)) +
-  geom_point(size = 2) +
-  geom_line(size = 0.9) +
+  geom_point(size = 1.6) +
+  geom_line(size = 0.8) +
   scale_y_continuous(labels = comma) +
   scale_color_brewer(palette = "Set1") +
   labs(x = "Anos", y = "Produção de café (t)",
        color = "País") +
-  theme_minimal(base_size = 15) +
-  theme(legend.justification = c(0.8, 0.5))
+  theme(legend.justification = c(0.5, 0.5)) +
+  theme_minimal(base_size = 13) 
 g2
 
 grid.arrange(g1, g2)
